d9d85fb0a79aa0ed3b0e2b0ac81bae068e4340db
juneTeams <junestudio0606@gmail.com>
2024-10-02 10:29:51 +0800

    this is Ch2

---
+++ src/Main.kt
@@ -0,0 +1,30 @@
+fun main(args: Array<String>) {
+    val char: Char = 'a'
+    val string: String = "hi my name is June"
+    val int: Int = 0
+    val float: Float = 0.0F
+    val double: Double = 0.0
+    val boolean: Boolean = false
+    println(char)
+    println(string)
+    println(int)
+    println(float)
+    println(double)
+    println(boolean)
+
+    //val and var
+
+
+    val charVal: Char = 'a'
+    var charVar: Char = 'a'
+//    charVal='b'
+//    charVar='b'
+    val stringVal = "hi"
+    var stringVar = "hi"
+//    stringVal="hi my name is June"
+//    stringVar="hi my name is June"
+    val intVal = 0
+    var intVar = 0
+//    intVal=1;
+//    intVar=1;
+}
